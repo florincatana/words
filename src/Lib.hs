a986a925a217118dee6ef0194d42a0b6f720bcca
Florin Catana <fcatana@montran.com>
2018-12-26 02:39:20 +0200

    added grid

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,35 @@
 module Lib
-    ( someFunc,
-    someString
+    ( grid
+    , languages
+    , outputGrid
+    , formatGrid
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn someString
+outputGrid :: [String] -> IO ()
+outputGrid = putStrLn (formatGrid grid)
 
-someString = "someString"
+formatGrid :: [String] -> String
+formatGrid = unlines
+
+grid = [ "__c________R___" 
+        ,"__SI________U__"
+        ,"__HASKELL____B_"
+        ,"__A___________Y"
+        ,"__R____________"
+        ,"__PHP__________"
+        ,"____S_LREP_____"
+        ,"____I____Y_____"
+        ,"____L____T_____"
+        ,"_________H_____"
+        ,"_________O_____"
+        ,"_________N_____"
+        ]
+
+languages = ["CHASRP"
+            , "RUBY"
+            , "HASKELL"
+            , "COBOL"
+            , "PYTHON"
+            , "PERL"
+            , "PHP"
+            ]
